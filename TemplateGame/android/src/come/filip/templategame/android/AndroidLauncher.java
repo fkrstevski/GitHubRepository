42a425d3c5530743c5c350b07374705f94ada8f1
Filip Krstevski <fkrstevski@gmail.com>
2015-02-06 16:47:59 -0500

    Add TemplateGame
    iOS, Android and Desktop only

---
+++ TemplateGame/android/src/come/filip/templategame/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package come.filip.templategame.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import come.filip.templategame.MyTemplateGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MyTemplateGame(), config);
+	}
+}
---
+++ TemplateGame/desktop/src/come/filip/templategame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package come.filip.templategame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import come.filip.templategame.MyTemplateGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyTemplateGame(), config);
+	}
+}

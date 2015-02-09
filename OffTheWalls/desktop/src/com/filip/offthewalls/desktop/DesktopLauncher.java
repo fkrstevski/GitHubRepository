d0bd72c627a9b8f9f028577a796deac1a0fd9a56
Filip Krstevski <fkrstevski@gmail.com>
2015-02-08 22:59:34 -0500

    OffTheWalls - initial commit

---
+++ OffTheWalls/desktop/src/com/filip/offthewalls/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.filip.offthewalls.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.filip.offthewalls.OffTheWallsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new OffTheWallsGame(), config);
+	}
+}

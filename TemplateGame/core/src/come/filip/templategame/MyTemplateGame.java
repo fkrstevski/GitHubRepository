c18567ae168dc42a81341ef708adefa6bf976e24
Filip Krstevski <fkrstevski@gmail.com>
2015-02-11 09:52:01 -0500

    TemplateGame
    Add CanyonBunny to this game

--- TemplateGame/core/src/come/filip/templategame/MyTemplateGame.java
+++ TemplateGame/core/src/come/filip/templategame/MyTemplateGame.java
@@ -1,27 +1,34 @@
 package come.filip.templategame;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.math.Interpolation;
+import come.filip.templategame.game.Assets;
+import come.filip.templategame.screens.DirectedGame;
+import come.filip.templategame.screens.MenuScreen;
+import come.filip.templategame.screens.transitions.ScreenTransition;
+import come.filip.templategame.screens.transitions.ScreenTransitionSlice;
+import come.filip.templategame.util.AudioManager;
+import come.filip.templategame.util.GamePreferences;
 
-public class MyTemplateGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class MyTemplateGame extends DirectedGame {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create () {
+        // Set Libgdx log level
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+
+        // Load assets
+        Assets.instance.init(new AssetManager());
+
+        // Load preferences for audio settings and start playing music
+        GamePreferences.instance.load();
+        AudioManager.instance.play(Assets.instance.music.song01);
+
+        // Start game at menu screen
+        ScreenTransition transition = ScreenTransitionSlice.init(2, ScreenTransitionSlice.UP_DOWN, 10, Interpolation.pow5Out);
+        setScreen(new MenuScreen(this), transition);
+    }
+
 }
--- TemplateGame/desktop/src/come/filip/templategame/desktop/DesktopLauncher.java
+++ TemplateGame/desktop/src/come/filip/templategame/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import come.filip.templategame.MyTemplateGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.height = 750;
+        config.width = 1334;
 		new LwjglApplication(new MyTemplateGame(), config);
 	}
 }

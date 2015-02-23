063ea7bb4e36dc0718de38b9beae6fcfe8d8d63d
Filip Krstevski <fkrstevski@gmail.com>
2015-02-22 22:23:26 -0500

    TheEdge - initial project

---
+++ TheEdge/android/src/com/filip/theedge/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.filip.theedge.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.filip.theedge.TheEdge;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TheEdge(), config);
+	}
+}

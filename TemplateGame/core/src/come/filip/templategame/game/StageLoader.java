7b054cf8674e2b8f93166bc83aa354db38f9301c
Filip Krstevski <fkrstevski@gmail.com>
2015-02-25 21:31:58 -0500

    TemaplateGame - remove code

--- TemplateGame/core/src/come/filip/templategame/game/StageLoader.java
+++ TemplateGame/core/src/come/filip/templategame/game/StageLoader.java
@@ -232,55 +232,6 @@ public class StageLoader
             zones.get(currentZone).AddStage(currentStage, stagePoints);
 
         }
-
-
-
-
-
-        /*Zone z2 = new Zone(0);
-        ArrayList<Vector2> stage1PointsZone2 = new ArrayList<Vector2>();
-        stage1PointsZone2.add(new Vector2(width * 0.15f, height * 0.85f));
-        stage1PointsZone2.add(new Vector2(width * 0.15f, height * 0.15f));
-        stage1PointsZone2.add(new Vector2(width * 0.85f, height * 0.15f));
-        z2.AddStage(0, stage1PointsZone2);
-
-        ArrayList<Vector2> stage2PointsZone2 = new ArrayList<Vector2>();
-        stage2PointsZone2.add(new Vector2(width * 0.15f, height * 0.15f));
-        stage2PointsZone2.add(new Vector2(width * 0.15f, height * 0.85f));
-        stage2PointsZone2.add(new Vector2(width * 0.85f, height * 0.85f));
-        z2.AddStage(1, stage2PointsZone2);
-
-        ArrayList<Vector2> stage3PointsZone2 = new ArrayList<Vector2>();
-        stage3PointsZone2.add(new Vector2(width * 0.15f, height * 0.85f));
-        stage3PointsZone2.add(new Vector2(width * 0.85f, height * 0.15f));
-        stage3PointsZone2.add(new Vector2(width * 0.85f, height * 0.85f));
-        z2.AddStage(2, stage3PointsZone2);
-
-        zones.add(z2);
-
-        Zone z3 = new Zone(1);
-        ArrayList<Vector2> stage1PointsZone3 = new ArrayList<Vector2>();
-        stage1PointsZone3.add(new Vector2(width * 0.15f, height * 0.85f));
-        stage1PointsZone3.add(new Vector2(width * 0.85f, height * 0.85f));
-        stage1PointsZone3.add(new Vector2(width * 0.15f, height * 0.15f));
-        stage1PointsZone3.add(new Vector2(width * 0.85f, height * 0.15f));
-        z3.AddStage(0, stage1PointsZone3);
-
-        ArrayList<Vector2> stage2PointsZone3 = new ArrayList<Vector2>();
-        stage2PointsZone3.add(new Vector2(width * 0.15f, height * 0.85f));
-        stage2PointsZone3.add(new Vector2(width * 0.15f, height * 0.15f));
-        stage2PointsZone3.add(new Vector2(width * 0.85f, height * 0.15f));
-        stage2PointsZone3.add(new Vector2(width * 0.85f, height * 0.85f));
-        z3.AddStage(1, stage2PointsZone3);
-
-        ArrayList<Vector2> stage3PointsZone3 = new ArrayList<Vector2>();
-        stage3PointsZone3.add(new Vector2(width * 0.15f, height * 0.85f));
-        stage3PointsZone3.add(new Vector2(width * 0.15f, height * 0.15f));
-        stage3PointsZone3.add(new Vector2(width * 0.50f, height * 0.5f));
-        stage3PointsZone3.add(new Vector2(width * 0.85f, height * 0.15f));
-        z3.AddStage(2, stage3PointsZone3);
-
-        zones.add(z3);*/
     }
 
     public static Zone getZone(int zoneID)

8eb25c1bc9cd6ef6f40db879609bb63176ceefd1
Filip Krstevski <fkrstevski@gmail.com>
2015-02-27 22:27:09 -0500

    TheEdge - initial commit with correct bundle id

---
+++ TheEdge/desktop/src/com/filip/edge/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.filip.edge.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.filip.edge.EdgeGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EdgeGame(), config);
+	}
+}

f86fb569c8a8801f4f6fc9cb38643433a4d8f35f
Filip Krstevski <fkrstevski@gmail.com>
2015-02-27 22:08:36 -0500

    TheEdge - initial commit

---
+++ TheEdge/ios/src/com/filip/thedge/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.filip.thedge;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.filip.thedge.TheEdgeGame;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new TheEdgeGame(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}

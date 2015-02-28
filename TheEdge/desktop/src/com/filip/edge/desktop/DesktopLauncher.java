e5727c474071374fcc3ebcfc3b92da3a5772d0ab
Filip Krstevski <fkrstevski@gmail.com>
2015-02-27 23:08:42 -0500

    Edge - get android running

--- TheEdge/desktop/src/com/filip/edge/desktop/DesktopLauncher.java
+++ TheEdge/desktop/src/com/filip/edge/desktop/DesktopLauncher.java
@@ -7,6 +7,8 @@ import com.filip.edge.EdgeGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new EdgeGame(), config);
+        config.height = 750;
+        config.width = 1334;
+		new LwjglApplication(new EdgeGame(null), config);
 	}
 }

8fa4f763e9ab1c0b509a64a1c91e96622dbe124f
Filip Krstevski <fkrstevski@gmail.com>
2015-10-18 12:06:02 -0400

    Delete old projects

--- OffTheWalls/core/src/com/filip/offthewalls/game/Shape.java
+++
@@ -1,9 +0,0 @@
-package com.filip.offthewalls.game;
-
-/**
- * Created by fkrstevski on 2015-02-10.
- */
-public abstract class Shape
-{
-    public abstract boolean containsBall(Ball ball, float levelMult);
-}
